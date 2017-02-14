cc516e5c01dbc62388f57d295bea103e62c47dc3
Rushali Paratey <rushaliy@gmail.com>
2017-02-13 23:16:30 -0500

    removed things

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 module.exports ={
-consumer_key:    'upzkHOkr638LjhT3BODanab8x'
-	, consumer_secret: '3mh2dpCyHRzGJj056ygLnwmLKjHpv23Vvj8DZG2328VFJkQS6s'
-	, access_token:  '827422322881679364-ycg71AH851Zgp5pn5emIG4lnN4oDS7e'
-	, access_token_secret: 'v0RFxgMxOfGevULNrq7Y7ab17l147CIt0c4gpaH5H4jh3'
+consumer_key:    ''
+	, consumer_secret: ''
+	, access_token:  ''
+	, access_token_secret: ''
 
 }
